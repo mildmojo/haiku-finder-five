808af61b027108b1b13ea13cf45f1c2e95c5447f
mildmojo <mildmojo@users.noreply.github.com>
2013-11-26 02:57:46 -0500

    Adds initial haiku scripts.
    `load_gcide.rb` will process GCIDE `CIDE.*` files and build a
    `words.sqlite3` DB for use by `find_haiku.rb`.
    
    `find_haiku.rb` will parse a .txt or .pdf file, split it roughly
    into English sentences, and look for haiku syllable patterns, dumping
    hits to the console.

---
+++ find_haiku.rb
@@ -0,0 +1,94 @@
+#!/bin/env ruby
+#
+# find_haiku.rb
+#
+# Just a hack to detect haiku among regular English text. Tags sentences with
+# exact 5/7/5 formation with 'PERFECT'. The underlying GCIDE dictionary doesn't
+# have accurate syllable info for all words and is notably missing conjugations
+# and plurals, so the script misses a lot and sometimes falsely identifies haiku
+# using those bad syllable counts. Needs a human proofreader right now.
+#
+# TODO: refactor this damned ugly code.
+#
+# Usage: find_haiku.rb <file.pdf|file.txt>
+#
+# Requires `xpdf` for the `pdftotext` program.
+# Requires a `words.sqlite3` DB with a `words (word, word_raw, syllable_count,
+#   is_accurate)` table built from GCIDE files. See `load_gcide.rb`.
+#
+require 'pathname'
+require 'sqlite3'
+require 'scalpel'
+
+INCLUDE_IMPERFECT = true
+puts 'Include imperfect haiku sentences: ' + INCLUDE_IMPERFECT.to_s
+
+class BadSyllablesError < StandardError; end
+
+db = SQLite3::Database.open('words.sqlite3')
+find_word = db.prepare 'SELECT * FROM words WHERE word = ?'
+
+def snarf words, syllable_count
+  line = []
+  syllables = 0
+  words.dup.each do |word|
+    line << word[:word]
+    syllables += word[:syllable_count]
+    words.shift
+    raise BadSyllablesError if syllables > syllable_count
+    return line.join(' ') if syllables == syllable_count
+  end
+  raise BadSyllablesError
+end
+
+file = ARGV.shift
+unless File.exist?(file.to_s)
+  STDERR.puts "Usage: #{$0} <file>"
+  exit 1
+end
+
+file = Pathname.new(file)
+
+case file.extname.downcase
+  when '.pdf'
+    `pdftotext -v 2>&1`
+    if $?.success?
+      text = `pdftotext "#{file}" -`
+    else
+      STDERR.puts 'Please install xpdf to process PDF files (pdftotext required)'
+      exit 1
+    end
+  when '.txt'
+    text = File.read(file)
+end
+
+sentences = Scalpel.cut(text)
+
+sentences.each do |sentence|
+  sentence = sentence.gsub(/[\n\r]/, ' ').gsub(/[^A-Za-z\s]/, '').gsub(/\s+/, ' ')
+  words = sentence.split(/\s+/)
+  words = words.map { |word|
+    stats = find_word.execute(word.downcase).to_a.first
+    { word: word,
+      syllable_count: stats ? stats[2] : 0 }
+  }
+  total_syllable_count = words.inject(0) { |sum, word| sum + word[:syllable_count] }
+  haiku = []
+  if words.all? { |word| word[:syllable_count] > 0 }
+    begin
+      haiku[0] = snarf(words, 5)
+      haiku[1] = snarf(words, 7)
+      haiku[2] = snarf(words, 5)
+    rescue BadSyllablesError
+      haiku = []
+    end
+
+    is_perfect = total_syllable_count == 17 && !haiku.empty?
+
+    if is_perfect || INCLUDE_IMPERFECT && !haiku.empty?
+      puts
+      puts 'PERFECT:' if total_syllable_count == 17 && INCLUDE_IMPERFECT
+      puts haiku.join("\n")
+    end
+  end
+end
---
+++ load_gcide.rb
@@ -0,0 +1,55 @@
+#!/bin/env ruby
+#
+# load_gcide.rb
+#
+# Lightning-fast loader to transform GCIDE dictionary quasi-XML into a syllable
+# count database. Reads `CIDE.*` and writes `words.sqlite3`.
+#
+# NOTE: GCIDE lacks conjugations, plurals, words, and sometimes falsely notates
+#   polysyllabic words as monosyllabic. User beware.
+#
+# Grab GCIDE data files at: http://ftp.gnu.org/gnu/gcide/
+#
+require 'sqlite3'
+
+db = SQLite3::Database.open('words.sqlite3')
+
+db.execute "PRAGMA synchronous=OFF"
+db.execute "PRAGMA count_changes=OFF"
+db.execute "PRAGMA journal_mode=MEMORY"
+db.execute "PRAGMA temp_store=MEMORY"
+db.execute "PRAGMA auto_vacuum=true"
+
+db.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS words (
+    word STRING,
+    word_raw STRING,
+    syllable_count INTEGER,
+    is_accurate BOOLEAN
+  );
+SQL
+db.execute 'DELETE FROM words;'
+stmt = db.prepare('INSERT INTO words VALUES (?, ?, ?, ?);')
+
+Dir['CIDE.*'].each do |file|
+  print "#{file}: "
+  doc = File.read(file).force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+  words = doc.scan(/<hw>(.*?)<\/hw>/).flatten.map(&:downcase).reject { |w| w.match(/[0-9]/) }
+  word_stats = words.map { |word|
+    [ word.gsub(/[^A-Za-z]/, ''),
+      word,
+      word.split(/[^A-Za-z]/).reject(&:empty?).length,
+      word.match(/[^A-Za-z]/) ? 1 : 0 ]
+  }
+  word_stats.uniq!(&:first)
+  puts "#{word_stats.length} words"
+  db.execute 'BEGIN TRANSACTION'
+  word_stats.each_with_index do |stats, idx|
+    print '.' if idx % 5 == 0
+    stmt.execute *stats
+  end
+  db.execute 'COMMIT TRANSACTION'
+  puts
+end
+
+db.execute 'CREATE INDEX word_index ON words (word);'
